3e2d89f153affa49b5ac474eb8ffe97e724b7f9c
Wooje <Wooje@users.noreply.github.com>
2018-05-13 16:17:27 +0900

    test scenes

---
+++ Assets/scripts/SpriteOutputSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteOutputSystem : MonoBehaviour
+{
+    //stat text
+    public TextAsset statfile;      //캐릭터 상태
+    public Text Name;
+    public string[] statLines;
+
+    // 현재와 끝 라인을 표시하는 int 변수
+    public int currentLine;
+    public int endAtLine;
+
+    public GameObject character;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (statfile != null)
+        {
+            statLines = statfile.text.Split('\n');
+        }
+        // text 파일이 몇 줄인지 계산.
+        if (endAtLine == 0)
+            endAtLine = statLines.Length - 1;
+
+        spriteRenderer = character.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Name.text.Contains("우제") == true)
+        {
+            //상태에 따라 캐릭터 스프라이트 불러오기
+            if (statLines[currentLine].Contains("nomal") == true)
+            {
+                spriteRenderer.sprite = sprite[0];
+                spriteRenderer.transform.position = character.transform.position;
+                spriteRenderer.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+            }
+            if (statLines[currentLine].Contains("smile") == true)
+            {
+                spriteRenderer.sprite = sprite[1];
+                spriteRenderer.transform.position = character.transform.position;
+                spriteRenderer.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+            }
+            if (statLines[currentLine].Contains("very_smile") == true)
+            {
+                spriteRenderer.sprite = sprite[2];
+                spriteRenderer.transform.position = character.transform.position;
+                spriteRenderer.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentLine += 1;
+        }
+    }
+}
---
+++ Assets/scripts/TextImporter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextImporter : MonoBehaviour {
+
+    //Text 공간 변수
+    public TextAsset textfile;      //캐릭터 대사
+
+    //Text 파일
+    public Text Text;
+    public Text Name;
+
+    //Text 줄 내용
+    [TextArea]  // text 개행 적용.
+    public string[] textLines;
+
+    // 현재 라인을 표시하는 int 변수
+    public int currentLine;
+
+
+
+    void Start ()
+    {       
+        //text 라인을 나누는 기준.
+        if (textfile != null)
+        {
+            textLines = textfile.text.Split('\n');
+        }
+
+        // text 창 초기화
+        Text.text = "";
+        StartCoroutine("Printing");             //코루틴 시작
+    }
+
+
+
+
+    void Update ()
+    {
+        // 터치시 코루틴 정지, 다음 라인으로 이동, 코루틴 다시 시작
+        if (Input.GetMouseButtonDown(0))
+        {
+            StopCoroutine("Printing");
+            Text.text = "";
+            currentLine += 1;
+            StartCoroutine("Printing");
+        }
+    }
+
+
+    IEnumerator Printing()
+    {
+        // ':' 문자의 인덱스를 복사 후, ':' 를 기준으로 이름과 대사를 나누고 대사는 한 글자씩 출력
+        int index = textLines[currentLine].IndexOf(":");
+        Name.text = textLines[currentLine].Substring(0, index);
+        for (int i = index + 1; i < textLines[currentLine].Length; i++)
+        {
+            Text.text += textLines[currentLine][i];
+            yield return new WaitForSeconds(0.1f);
+        }
+        StopCoroutine("Printing");          // text 가 모두 출력되면 코루틴 정지
+    }
+}
